42667e2229a6d92643ad8ff572a6dd1a6f8799e8
Sanders Gutierrez <ing.sanders@gmail.com>
2020-08-14 07:58:58 -0300

    Control de estado por el componente padre.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,37 @@ import "./index.css";
 const appRoot = document.querySelector("#root");
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button
-                className="square"
-                onClick={() => this.setState({ value: "X" })}
-            >
-                {this.state.value}
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        console.log(squares);
+        squares[i] = "X";
+        this.setState({ squares: squares });
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
