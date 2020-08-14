2d11fdb9c3007b3ee950438acdc44e56c940406e
Sanders Gutierrez <ing.sanders@gmail.com>
2020-08-14 07:18:08 -0300

    Tablero listo

--- src/index.js
+++ src/index.js
@@ -4,31 +4,71 @@ import "./index.css";
 
 const appRoot = document.querySelector("#root");
 
-class ShoppingList extends React.Component {
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <div className="shopping-list">
-                <h1>Shopping List for {this.props.name}</h1>
-                <ul>
-                    <li>Instagram</li>
-                    <li>Whatsapp</li>
-                    <li>Oculus</li>
-                </ul>
-                <MyForm />
+            <button
+                className="square"
+                onClick={() => this.setState({ value: "X" })}
+            >
+                {this.state.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = "Next player: x";
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
         );
     }
 }
 
-class MyForm extends React.Component {
+class Game extends React.Component {
     render() {
-        return React.createElement(
-            "div",
-            { className: "my-form" },
-            React.createElement("input", { placeholder: "Escriba su nombre" }),
-            React.createElement("button", {}, "Enviar datos")
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
         );
     }
 }
 
-ReactDOM.render([<ShoppingList />, <MyForm />], appRoot);
+ReactDOM.render(<Game />, appRoot);
